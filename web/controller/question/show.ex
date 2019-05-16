4167b334d0775be5398530c80297b9f066acd74b
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-16 14:16:01 +0900

    Updated /web/controller/quesiton/show.ex

--- web/controller/question/show.ex
+++ web/controller/question/show.ex
@@ -1,7 +1,23 @@
 defmodule StackoverflowCloneG.Controller.Question.Show do
   use StackoverflowCloneG.Controller.Application
 
-  def show(_conn) do
-    # Implement me
+  def show(%Antikythera.Conn{request: %Antikythera.Request{path_matches: %{id: id}}} = conn) do
+    req = Dodai.RetrieveDedicatedDataEntityRequest.new(
+      StackoverflowCloneG.Dodai.default_group_id(),
+      "Question",
+      id,
+      StackoverflowCloneG.Dodai.root_key())
+
+    res = Sazabi.G2gClient.send(
+      conn.context,
+      StackoverflowCloneG.Dodai.app_id(),
+      req)
+
+    IO.inspect res.body
+    res_body = %{
+      Title: res.body["data"]["title"],
+      Body: res.body["data"]["body"],
+    }
+    Conn.json(conn, 200, res_body)
   end
 end
