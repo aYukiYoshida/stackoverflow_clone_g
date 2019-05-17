742bfcf8919d6d98a0ccd0a939b16b76b1932e76
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-17 15:03:07 +0900

    Updated test/web/controller/answer/index_test.exs

--- test/web/controller/answer/index_test.exs
+++ test/web/controller/answer/index_test.exs
@@ -1,7 +1,24 @@
 defmodule StackoverflowCloneG.Controller.Answer.IndexTest do
   use StackoverflowCloneG.CommonCase
+  alias StackoverflowCloneG.TestData.AnswerData
 
-  test "create/1 " do
-    # Implement me
+  @api_prefix "/v1/answer"
+
+  test "index/1 " <>
+    "should return answers"do
+    :meck.expect(Sazabi.G2gClient, :send, fn(_, _, req) ->
+      assert req.query == %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
+        limit: nil,
+        skip:  nil,
+        query: %{},
+        sort:  %{"_id" => 1}
+      }
+
+      %Dodai.RetrieveDedicatedDataEntityListSuccess{body: [AnswerData.dodai()]}
+    end)
+
+    res = Req.get(@api_prefix)
+    assert res.status               == 200
+    assert Poison.decode!(res.body) == [AnswerData.gear()]
   end
 end
