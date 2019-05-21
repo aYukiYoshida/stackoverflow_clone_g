5691e73003bc71ddb8b13a6822b089c3df0e79a8
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-21 15:49:13 +0900

    Updated /answer/create_test.exs

--- test/web/controller/answer/create_test.exs
+++ test/web/controller/answer/create_test.exs
@@ -35,6 +35,28 @@ defmodule StackoverflowCloneG.Controller.Answer.CreateTest do
     assert Poison.decode!(res.body) == AnswerData.gear()
   end
 
+  test "create/1 when request body is invalid " <>
+    "it returns BadRequestError" do
+    :meck.expect(StackoverflowCloneG.Plug.FetchMe, :fetch, fn(conn, _) ->
+      Antikythera.Conn.assign(conn, :me, UserData.dodai())
+    end)
+    invalid_bodies = [
+      %{                    "question" => "question_id"},
+      %{"body" => "",       "question" => "question_id"},
+      %{"body" => "本文"},
+      %{"body" => "本文",    "question" => ""},
+    ]
+    Enum.each(invalid_bodies, fn body ->
+      res = Req.post_json(@api_prefix, body, @header)
+      assert res.status               == 400
+      assert Poison.decode!(res.body) == %{
+        "code"        => "400-06",
+        "description" => "Unable to understand the request.",
+        "error"       => "BadRequest",
+      }
+    end)
+  end
+
   test "create/1 when user did not login " <>
     "it returns InvalidCredential" do
     res = Req.post_json(@api_prefix, @body, @header)
