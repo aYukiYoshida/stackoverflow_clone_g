defddfc9ea54ddb252d6e156894fbb78a00061ab
aYukiYoshida <yuki.yoshida@access-company.com>
2019-05-21 17:41:30 +0900

    implement of test on comment

--- test/web/controller/comment/create_test.exs
+++ test/web/controller/comment/create_test.exs
@@ -1,7 +1,12 @@
 defmodule StackoverflowCloneG.Controller.Comment.CreateTest do
   use StackoverflowCloneG.CommonCase
+  alias StackoverflowCloneG.TestData.UserData
 
-  test "create/1 " do
-    # Implement me
+  test "create/1 " <> "should create question" do
+    :meck.expect(StackoverflowCloneG.Plug.FetchMe, :fetch, fn(conn, _) ->
+      Antikythera.Conn.assign(conn, :me, UserData.dodai())
+    end)
+
+
   end
 end
