57e2083d45ba6ea90bf3c019df3559935d5c57db
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-27 16:23:41 +0900

    web/static/components/Answer.vue

--- web/static/components/Answer.vue
+++ web/static/components/Answer.vue
@@ -3,6 +3,30 @@
     {{ answer.body }}<br>
     {{ answer.createdAt }}&nbsp;&nbsp; {{ answer.userId }}&nbsp;<br>
     <br>
+    <form
+      class="data-form"
+      @submit.prevent="submit"
+    >
+      <div class="form-group">
+          <label for="form-body">コメントを追加</label>
+          <textarea
+            id="form-body"
+            v-model="commentBody"
+            class="body-edit form-control"
+            minlength="1"
+            maxlength="50"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <button
+            class="btn btn-primary mb-2"
+            type="submit"
+          >
+            投稿
+          </button>
+        </div>
+      </form>
   </div>
 </template>
 
@@ -18,6 +42,19 @@ export default {
     answer: {
       type: Object,
       required: true,
+    },
+  },
+  data() {
+    return {
+      commentBody: '',
+    };
+  },
+  methods: {
+    submit() {
+      this.$store.dispatch('createAnswerComment', { questionId: this.$route.params.id, answerId: this.answer.id, body: this.commentBody })
+      .then(() => {
+        location.reload();
+      });
     },
   },
 };
