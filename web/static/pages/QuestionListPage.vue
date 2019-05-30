8d9bd3e7f297db2a1b019d1dffc32447e3442403
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-30 10:44:15 +0900

    質問タイトル検索機能を追加

--- web/static/pages/QuestionListPage.vue
+++ web/static/pages/QuestionListPage.vue
@@ -3,19 +3,47 @@
     <div class="page-title">
       <h1>質問を見る</h1>
     </div>
-    <div class="question-search">
-      <p>検索したいキーワードを入力してください。</p>
-      <input type="search" name="search" placeholder="タイトルを入力">
-      <input type="search" name="search" placeholder="質問内容を入力">
-      <input type="submit" name="submit" value="Search">
-    </div>
+    <form
+      class="data-form"
+      @submit.prevent="query"
+    >
+      <div class="form-group">
+        <label for="form-body">質問タイトルを検索</label>
+        <textarea
+          id="form-body"
+          v-model="queryBody"
+          class="body-edit form-control"
+          minlength="1"
+          maxlength="50"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <button
+          class="btn btn-primary mb-2"
+          type="submit"
+        >
+          検索
+        </button>
+        <button
+          class="btn btn-primary mb-2"
+          type="submit"
+          @click.prevent="clear"
+        >
+          クリア
+        </button>
+      </div>
+    </form>
     <div v-if="isLoggedIn()">
       <router-link to="question/create">
         質問する
       </router-link>
     </div>
-    <div v-if="!isLoggedIn()" class="animated infinite bounce">
-      <router-link 
+    <div
+      v-if="!isLoggedIn()"
+      class="animated infinite bounce"
+    >
+      <router-link
         to="/login"
         class="error-message"
       >
@@ -23,22 +51,43 @@
       </router-link>
     </div>
     <hr>
-    <div
-      v-for="question in questions"
-      :key="question.id"
-    >
-      <h5 class="title">
-        <router-link :to="{ name: 'QuestionDetailPage', params: { id: question.id }}">
-          {{ question.title }}
-        </router-link>
-      </h5>
-      <div class="additional">
-        Posted at {{ question.createdAt }}
-        by <router-link :to="{ name: 'UserDetailPage', params: { id: question.userId }}">
-          {{ question.userId }}
-        </router-link>
+    <div v-if="resultBody==''">
+      <div
+        v-for="question in questions"
+        :key="question.id"
+      >
+        <h5 class="title">
+          <router-link :to="{ name: 'QuestionDetailPage', params: { id: question.id }}">
+            {{ question.title }}
+          </router-link>
+        </h5>
+        <div class="additional">
+          Posted at {{ question.createdAt }}
+          by <router-link :to="{ name: 'UserDetailPage', params: { id: question.userId }}">
+            {{ question.userId }}
+          </router-link>
+        </div>
+        <hr>
       </div>
-      <hr>
+    </div>
+    <div v-else>
+      <div
+        v-for="ques in resultBody"
+        :key="ques.id"
+      >
+        <h5 class="title">
+          <router-link :to="{ name: 'QuestionDetailPage', params: { id: ques.id }}">
+            {{ ques.title }}
+          </router-link>
+        </h5>
+        <div class="additional">
+          Posted at {{ ques.createdAt }}
+          by <router-link :to="{ name: 'UserDetailPage', params: { id: ques.userId }}">
+            {{ ques.userId }}
+          </router-link>
+        </div>
+        <hr>
+      </div>
     </div>
   </div>
 </template>
@@ -47,6 +96,14 @@
 
 export default {
   name: 'QuestionListPage',
+  data() {
+    return {
+      queryBody: '',
+      resultBody: [],
+      c: 0,
+      l: 0,
+    };
+  },
   computed: {
     questions() {
       return this.sortBy(this.$store.state.questions, 'createdAt').reverse();
@@ -58,6 +115,17 @@ export default {
   methods: {
     retrieveQuestions() {
       this.$store.dispatch('retrieveQuestions');
+    },
+    query() {
+      this.resultBody = [];
+      for (this.c = 0, this.l = (this.questions).length; this.c < this.l; this.c = this.c + 1) {
+        if (this.questions[this.c].title.match(this.queryBody)) {
+          this.resultBody.push(this.questions[this.c]);
+        }
+      }
+    },
+    clear() {
+      this.resultBody = [];
     },
   },
 };
